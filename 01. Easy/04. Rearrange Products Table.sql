d218cde37886d5a7710bfcbf2aa640a09eb6f7f9
EX1ST3NCE <prasadnitin05@gmail.com>
2022-07-02 15:39:56 +0530

    Final Commit

--- 01. Easy/04. Rearrange Products Table.sql
+++ 01. Easy/04. Rearrange Products Table.sql
@@ -1,6 +1,12 @@
 -- https://leetcode.com/problems/rearrange-products-table/
 
 /*
-
-
+Product_id, store
+price IS NOT NULL
 */
+
+SELECT product_id, 'store1' AS store, store1 AS Price FROM Products WHERE store1 IS NOT NULL
+UNION
+SELECT product_id, 'store2' AS store, store2 AS Price FROM Products WHERE store2 IS NOT NULL
+UNION
+SELECT product_id, 'store3' AS store, store3 AS Price FROM Products WHERE store3 IS NOT NULL
